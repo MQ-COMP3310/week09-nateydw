843eb7d5b78a15a1b0ee181e8f810eebdde52185
nateydw <natebro333@gmail.com>
2025-05-08 13:56:41 +1000

    changes to auth.py

--- project/auth.py
+++ project/auth.py
@@ -18,14 +18,11 @@ def login_post():
 
     user = User.query.filter_by(email=email).first()
 
-    # check if the user actually exists
-    # take the user-supplied password and compare it with the stored password
     if not user or not (user.password == password):
         flash('Please check your login details and try again.')
         app.logger.warning("User login failed")
-        return redirect(url_for('auth.login')) # if the user doesn't exist or password is wrong, reload the page
+        return redirect(url_for('auth.login'))
 
-    # if the above check passes, then we know the user has the right credentials
     login_user(user, remember=remember)
     return redirect(url_for('main.profile'))
 
@@ -39,16 +36,14 @@ def signup_post():
     name = request.form.get('name')
     password = request.form.get('password')
 
-    user = db.session.execute(text('select * from user where email = "' + email +'"')).all()
-    if len(user) > 0: # if a user is found, we want to redirect back to signup page so user can try again
-        flash('Email address already exists')  # 'flash' function stores a message accessible in the template code.
+    user = User.query.filter_by(email=email).all()
+    if len(user) > 0:
+        flash('Email address already exists')
         app.logger.debug("User email already exists")
         return redirect(url_for('auth.signup'))
 
-    # create a new user with the form data. TODO: Hash the password so the plaintext version isn't saved.
     new_user = User(email=email, name=name, password=password)
 
-    # add the new user to the database
     db.session.add(new_user)
     db.session.commit()
 
@@ -59,5 +54,3 @@ def signup_post():
 def logout():
     logout_user();
     return redirect(url_for('main.index'))
-
-# See https://www.digitalocean.com/community/tutorials/how-to-add-authentication-to-your-app-with-flask-login for more information
